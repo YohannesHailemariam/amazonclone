d2e1ef4a2888b86fa29f34adf916a2fef3f9f0b3
iamjohnhm <jhm90822@gmail.com>
2024-03-28 06:51:46 +0300

    Created the banner section of the React app using some web dev't materials like React Slick and the banner contains multiple photo displayed as a slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import Banner from "./components/home/Banner";
 
 
 function App() {
   return (
     <div className="font-bodyFont">
       <Header />
+      <Banner />
     </div>
   );
 }

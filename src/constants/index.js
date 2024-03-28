75d066a7bf9e1c614bd9fe0a45a9f867f160b94c
iamjohnhm <jhm90822@gmail.com>
2024-03-28 23:07:42 +0300

    Created the footer section of the webpage, the footer is divided into three sections which are the top, the middle and the bottom sections.

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,3 +16,242 @@ export const allItems = [
     { _id:"126", title:"Video Games"},
     { _id:"127", title:"Women's Fashion"}
 ];
+
+export const middlelist = [
+{
+    _id: 2221,
+    title: "Get to Know Us",
+    ListItem: [
+        {
+            _id: "001",
+            ListData: [
+                "Careers",
+                "Blog",
+                "About Amazon",
+                "Investor Relations",
+                "Amazon Devices",
+                "Amazon Science",
+            ],
+        },
+    ],
+},
+
+{
+    _id: 2222,
+    title: "Make Money with Us",
+    ListItem: [
+        {
+          _id: "002",
+          ListData: [
+            "Sell products on Amazon",
+            "Sell on Amazon Buisness",
+            "Sell apps on Amazon",
+            "Become and Affiliate",
+            "Advertise Your Products",
+            "Sell Product with Us",
+            "Host an Amazon Hub",
+            "See More Make Money with Us",
+          ],  
+        },
+    ],
+},
+
+{
+    _id: 2223  ,
+    title: "Make Money with Us",
+    ListItem: [
+        {
+          _id: "003",
+          ListData: [
+            "Amazon Buisness Card",
+            "Shop with Points",
+            "Reload Your Balance",
+            "Amazon Currency Converter",
+          ],  
+        },
+    ],
+},
+
+{
+    _id: 2224,
+    title: "Let Us Help You",
+    ListItem: [
+        {
+          _id: "004",
+          ListData: [
+            "Amazon and COVID-19",
+            "Your Account",
+            "Your Orders",
+            "Shipping Rates & Policies",
+            "Returns & Replacements",
+            "Manage Your Content and Devices",
+            "Amazon Assistant",
+            "FAQ & Help",
+          ],  
+        },
+    ],
+},
+]
+
+export const footerBottomItem = [
+    {
+        _id: 1001,
+        title: "Amazon Music",
+        des: "Stream millions of songs",
+    },
+    {
+        _id: 1002,
+        title: "Amazon Advertising",
+        des: "Find, attract, and engage customers",
+    },
+    {
+        _id: 1003,
+        title: "Amazon Drive",
+        des: "Cloud storage from Amazon",
+    },
+    {
+        _id: 1004,
+        title: "6pm",
+        des: "Score deals on fashion brands",
+    },
+    {
+        _id: 1005,
+        title: "AbeBooks",
+        des: "Books, art & collectibles",
+    },
+    {
+        _id: 1006,
+        title: "ACX",
+        des: "Audiobook Publishing Made Easy",
+    },
+    {
+        _id: 1007,
+        title: "Sell on Amazon",
+        des: "Start a Selling Account",
+    },
+    {
+        _id: 1008,
+        title: "Amazon Buisness",
+        des: "Everything For Your Buisness",
+    },
+    {
+        _id: 1009,
+        title: "AmazonGlobal",
+        des: "Ship Orders Internationally",
+    },
+    {
+        _id: 1010,
+        title: "Home Services",
+        des: "Experienced Pros appiness Guarantee",
+    },
+    {
+        _id: 1011,
+        title: "Amazon Ignite",
+        des: "Sell you original Digital Educational Resources",
+    },
+    {
+        _id: 1012,
+        title: "Amazon Web Services",
+        des: "Scalable Cloud Computing Services",
+    },
+    {
+        _id: 1013,
+        title: "Audible",
+        des: "Listen to Books & Original Audio performances",
+    },
+    {
+        _id: 1014,
+        title: "Book Depository",
+        des: "Books With Free Delivery Worldwide",
+    },
+    {
+        _id: 1015,
+        title: "Boc Office Mojo",
+        des: "Find Movie Box Office Data",
+    },
+    {
+        _id: 1016,
+        title: "ComiXology",
+        des: "Thousands of Digital Comics",
+    },
+    {
+        _id: 1017,
+        title: "DPReview",
+        des: "Digital Photography",
+    },
+    {
+        _id: 1018,
+        title: "Fabric",
+        des: "Sewing, Quilting & Knitting",
+    },
+    {
+        _id: 1019,
+        title: "Goodreads",
+        des: "Book reviews & recommendations",
+    },
+    {
+        _id: 1020,
+        title: "IMDb",
+        des: "Movies, Tv & Celebrities",
+    },
+    {
+        _id: 1021,
+        title: "IMDbPro",
+        des: "Get Info Entertainment Professionals",
+    },
+    {
+        _id: 1022,
+        title: "Kindle Direct Publishing",
+        des: "Indie Digital & Print Publishing Made Easy",
+    },
+    {
+        _id: 1023,
+        title: "Prime Video Direct",
+        des: "Video Distribution Made Easy",
+    },
+    {
+        _id: 1024,
+        title: "Shopbop",
+        des: "Designer Fashion Brands",
+    },
+    {
+        _id: 1025,
+        title: "Woot!",
+        des: "Deals and Shenanigans",
+    },
+    {
+        _id: 1026,
+        title: "Zappons",
+        des: "Shoes & Clothing",
+    },
+    {
+        _id: 1027,
+        title: "Ring",
+        des: "Smart Home Security Systems",
+    },
+    {
+        _id: 1028,
+        title: "eero WiFi",
+        des: "Stream 4K Video in Every Room",
+    },
+    {
+        _id: 1029,
+        title: "Neighbors App",
+        des: "Smart Security for Every Home",
+    },
+    {
+        _id: 1030,
+        title: "Neighbor App",
+        des: "Real-Time Crime & Safety Alerts",
+    },
+    {
+        _id: 1031,
+        title: "Amazon Subscription Boxes",
+        des: "Top subscription boxes - right to you doo",
+    },
+    {
+        _id: 1032,
+        title: "PillPack",
+        des: "Pharmacy Simplified",
+    },
+]
